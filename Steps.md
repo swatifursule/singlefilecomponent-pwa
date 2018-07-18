983cc2310615be16794629aabc0cb6f187a6287e
Swati Fursule <swati@silvertrak.com.au>
2018-07-18 15:48:22 +1000

    simple to do application

---
+++ Steps.md
@@ -0,0 +1,9 @@
+npm install -g vue-cli
+
+vue init pwa singlefilecomponent-pwa
+
+added node-sass and sass-loader npm libraries
+
+npm install
+
+npm run dev
---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <header>
+      <span>Vue.js PWA</span>
+    </header>
+    <main>
+      <img src="./assets/logo.png" alt="Vue.js PWA">
+    </main>
+    <h1>My Todo App!</h1>
+	<TodoList/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+	components: {
+		TodoList
+	}
+}
+</script>
+
+<style lang="scss">
+@import './variables.scss';
+
+*, *::before, *::after {
+	box-sizing: border-box;
+}
+
+#app {
+	max-width: 400px;
+	margin: 0 auto;
+	line-height: 1.4;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: $vue-blue;
+}
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+
+h1 {
+	text-align: center;
+}
+</style>
+
